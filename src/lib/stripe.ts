d67a5b36c3fe97a3c786fa4ff3178f834ea4122d
evantaylaz-cyber <evan.tay.laz@gmail.com>
2026-01-20 08:55:58 -0800

    Update PREMIUM_PRICE_ID environment variable

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,8 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2023-10-16",
-      typescript: true,
+  apiVersion: "2023-10-16",
+  typescript: true,
 });
 
-export const PREMIUM_PRICE_ID = process.env.STRIPE_PREMIUM_PRICE_ID!;
+export const PREMIUM_PRICE_ID = process.env.STRIPE_PRICE_ID!;
